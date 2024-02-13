af8afd0f5e4362affd5f08d9c025f3c857d4256f
oishi-yuma-99 <138931627+oishi-yuma-99@users.noreply.github.com>
2024-02-13 08:19:01 +0000

    [Finish]4-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :post_images, only: [:new, :index, :show]
   devise_for :users
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
