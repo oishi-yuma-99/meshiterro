cf0660bc7d76d212a3aefbfe138674e7b38e62de
oishi-yuma-99 <138931627+oishi-yuma-99@users.noreply.github.com>
2024-02-14 10:07:27 +0000

    [Finish]4-18

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -9,15 +9,16 @@ class PostImagesController < ApplicationController
     @post_image.save
     redirect_to post_images_path
   end
-  
+
   def index
     @post_images = PostImage.all
   end
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
-  
+
   def destroy
     post_image = PostImage.find(params[:id])
     post_image.destroy
@@ -28,5 +29,5 @@ class PostImagesController < ApplicationController
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-  
+
 end
